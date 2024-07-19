3e973b461751bfbad45795871847cfad0fa0efbd
sophiashe123456 <zhiyang.she@student.adelaide.edu.au>
2024-07-19 11:41:21 +0930

    1

--- client.py
+++ client.py
@@ -1,62 +1,83 @@
-import socket
-import threading
-import os
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-def receive_messages(client_socket):
+import asyncio
+import websockets
+
+
+async def send_message(websocket):
+    while True:
+        message = input("Enter message =====> : ")
+        # if message.startswith("/file"):
+        #     parts = message.split(' ', 3)
+        #     filename = parts[2]
+        #     with open(filename, "rb") as f:
+        #         file_data = f.read().decode('latin1')
+        #     message = f"/file {parts[1]} {filename} {file_data}"
+        await websocket.send(message)
+        if message == "/quit":
+            break
+
+
+async def receive_message(websocket):
     while True:
         try:
-            message = client_socket.recv(1024)
-            if not message:
-                break
-            print(message.decode())
-        except:
+            response = await websocket.recv()
+            print(f"\nReceived: {response}")
+        except websockets.ConnectionClosed:
+            print("Connection closed by server~~.")
             break
 
-def send_file(client_socket, filename, recipient):
-    if not os.path.isfile(filename):
-        print(f"File '{filename}' does not exist.")
-        return
 
-    with open(filename, "rb") as file:
-        client_socket.send(f"/file {recipient} {filename}".encode())
-        data = file.read()
-        client_socket.send(data)
-
-def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = input("Enter the server IP address: ")
-    server_port = int(input("Enter the server port: "))
-    client.connect((server_ip, server_port))
-
-    username = input("Enter your username: ")
-    client.send(username.encode())
-
-    threading.Thread(target=receive_messages, args=(client,)).start()
-
+async def handle_user_input(websocket):
     while True:
-        try:
-            message = input()
-            if message.startswith("/file"):
-                parts = message.split(maxsplit=2)
-                if len(parts) < 3:
-                    print("Usage: /file [recipient] [filename]")
-                    continue
-                _, recipient, filename = parts
-                send_file(client, filename, recipient)
-            elif message == "/quit":
-                client.send(message.encode())
-                print("You have left the chat.")
-                break
-            else:
-                client.send(message.encode())
-        except BrokenPipeError:
-            print("Connection to the server was lost.")
-            break
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        message = input("Enter your message (or quit to disconnect): ")
+        await websocket.send(message)
+        response = await websocket.recv()
+        print(response)
+        if message == "/quit":
             break
 
-    client.close()
+async def main():
+    uri = "ws://localhost:8767"
+    exit_flag = 0
+    while True:
+        username = input("Enter your username: ")
+        async with websockets.connect(uri) as websocket:
+            # while True:
+            # username = input("Enter your username: ")
+            await websocket.send(username)
+            response = await websocket.recv()
+            if response.startswith("ERROR"):
+                print(response)
+                continue
+            elif response.startswith("SUCCESS"):
+                print(response)
+                # send_task = asyncio.create_task(send_message(websocket))
+                # receive_task = asyncio.create_task(receive_message(websocket))
+                #
+                # await asyncio.gather(send_task, receive_task)
+                # break
+                # await handle_user_input(websocket)
+
+
+                while True:
+                    try:
+                        # message = input()
+                        message = input("Enter message to send (or 'quit' to quit): ")
+                        if message == '/quit':
+                            exit_flag = 1
+                            break
+                        await websocket.send(message)
+                        response = await websocket.recv()
+                        print(response)
+                    except websockets.exceptions.ConnectionClosed:
+                        exit_flag = 1
+                        print("Connection closed by server.")
+                        break
+
+            if exit_flag:
+                break
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
